25a1b37d6c93ba087a28b4c99e051df5bd34b1ae
joostyn <justyn.helgeson@gmail.com>
2024-04-14 17:40:45 -0500

    Inserted index.js file and successfully connected to mongoDB

---
+++ static/js/index.js
@@ -0,0 +1,22 @@
+
+// Documentation used for this 'https://www.mongodb.com/docs/drivers/node/current/quick-start/connect-to-mongodb/'
+
+
+const { MongoClient } = require("mongodb");
+// Connection string for mongodb
+const uri = 'mongodb://localhost:27017/';
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const database = client.db('birth_data');
+    const states = database.collection('states');
+    // Query for a player named 'Michael Jordan'
+    const query = { name: 'Michael Jordan' };
+    const result = await states.findOne(query);
+    console.log(result);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
